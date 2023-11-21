d34f3a864f4f5d432c30ac568d49f62e32282c68
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-11-21 22:06:47 +0700

    remove module-info.java rename main class

--- src/main/java/com/example/oop/MainApplication.java
+++ src/main/java/com/example/oop/MainApplication.java
@@ -1,14 +1,18 @@
 package com.example.oop;
 
+import com.example.oop.model.ModelTwitter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 
 public class MainApplication extends Application {
+    public static List<ModelTwitter> twitterPosts;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/com/example/oop/home-page.fxml"));
