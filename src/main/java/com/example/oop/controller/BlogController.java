d9006d829910fd680b3dc034d5b55a9f7dbe214a
bhtu <bhtu.work@gmail.com>
2023-12-10 14:37:03 +0700

    Blog UI

--- src/main/java/com/example/oop/controller/BlogController.java
+++ src/main/java/com/example/oop/controller/BlogController.java
@@ -5,21 +5,45 @@ import com.example.oop.service.CrawlerBlog;
 import com.example.oop.views.HomeView;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class BlogController {
+public class BlogController implements Initializable {
     @FXML
-    private Label json;
+    private TableView<ModelBlog> tableViewMain;
+    @FXML
+    private TableColumn<ModelBlog, Long> tableColumnIndex;
+    @FXML
+    private TableColumn<ModelBlog, Text> tableColumnName;
+    @FXML
+    private TableColumn<ModelBlog, Hyperlink> tableColumnUrlImage;
+    @FXML
+    private TableColumn<ModelBlog, Hyperlink> tableColumnUrlBlog;
+
 
     @FXML
-    void buttonBack(ActionEvent event) throws IOException {
-        ((Stage) json.getScene().getWindow()).setScene(new HomeView().getScene());
+    void buttonBack(ActionEvent ignored) throws IOException {
+        ((Stage) tableViewMain.getScene().getWindow()).setScene(new HomeView().getScene());
     }
 
     @FXML
@@ -30,9 +54,76 @@ public class BlogController {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String jsonLog = gson.toJson(list);
 
-        // In chuỗi JSON ra màn hình
-        System.out.println(json);
+        this.tableViewMain.setItems(FXCollections.observableList(list));
 
-        json.setText(jsonLog);
+        // In chuỗi JSON ra màn hình
+        System.out.println(jsonLog);
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // index
+        this.tableColumnIndex.setCellValueFactory(cellData -> {
+            long index = tableViewMain.getItems().indexOf(cellData.getValue()) + 1;
+            return new ReadOnlyObjectWrapper<>(index);
+        });
+
+        // name
+        this.tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        // urlImage
+        this.tableColumnUrlImage.setCellValueFactory(cellData -> {
+            var link = new Hyperlink(cellData.getValue().getUrlImage());
+            link.setOnAction(e -> {
+                try {
+                    Desktop.getDesktop().browse(URI.create(link.getText()));
+                } catch (IOException ex) {
+                    Stage popup = new Stage();
+                    // block if not closed
+                    popup.initModality(Modality.APPLICATION_MODAL);
+                    popup.setTitle("Link Error");
+
+                    javafx.scene.control.Label errMessage = new Label(ex.getMessage());
+                    // TODO: style
+
+                    var popupRoot = new StackPane(errMessage);
+
+                    var popupScene = new Scene(popupRoot);
+
+                    popup.setScene(popupScene);
+                    popup.showAndWait();
+                }
+            });
+            return new ReadOnlyObjectWrapper<>(link);
+
+        });
+
+        // urlBlog
+        this.tableColumnUrlBlog.setCellValueFactory(cellData -> {
+            var link = new Hyperlink(cellData.getValue().getUrlBlog());
+            link.setOnAction(e -> {
+                try {
+                    Desktop.getDesktop().browse(URI.create(link.getText()));
+                } catch (IOException ex) {
+                    Stage popup = new Stage();
+                    // block if not closed
+                    popup.initModality(Modality.APPLICATION_MODAL);
+                    popup.setTitle("Link Error");
+
+                    javafx.scene.control.Label errMessage = new Label(ex.getMessage());
+                    // TODO: style
+
+                    var popupRoot = new StackPane(errMessage);
+
+                    var popupScene = new Scene(popupRoot);
+
+                    popup.setScene(popupScene);
+                    popup.showAndWait();
+                }
+            });
+            return new ReadOnlyObjectWrapper<>(link);
+
+        });
     }
 }
